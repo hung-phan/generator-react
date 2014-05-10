497de68877d35306e68434ad243539ec2a3d6cbf
hung-phan <colorvisavn@gmail.com>
2014-05-10 14:34:50 +0700

    Add template logic

--- component/index.js
+++ component/index.js
@@ -12,6 +12,53 @@ var ComponentGenerator = module.exports = function ComponentGenerator(args, opti
 
 util.inherits(ComponentGenerator, yeoman.generators.NamedBase);
 
+ComponentGenerator.prototype.askForModuleLoader = function askForModuleLoader() {
+  var cb = this.async();
+
+  var prompts = [{
+    type: 'list',
+    name: 'moduleLoader',
+    message: 'What module loader would you like to include?',
+    choices: [{
+      name: 'Browserify',
+      value: 'browserify'
+    }, {
+      name: 'Requirejs',
+      value: 'requirejs'
+    }]
+  }];
+
+  this.prompt(prompts, function(props) {
+    this.moduleLoader = props.moduleLoader;
+    cb();
+  }.bind(this));
+};
+
+ComponentGenerator.prototype.askForJSFile = function askForJSFile() {
+  var cb = this.async();
+
+  var prompts = [{
+    type: 'checkbox',
+    name: 'jsFile',
+    message: 'What utils would you like to include?',
+    choices: [{
+      name: 'React Addons',
+      value: 'includeReactAddons',
+      checked: true
+    }]
+  }];
+
+  this.prompt(prompts, function(props) {
+    function includeJS(js) {
+      return props.jsFile.indexOf(js) !== -1;
+    }
+
+    // JS
+    this.includeReactAddons = includeJS('includeReactAddons');
+    cb();
+  }.bind(this));
+};
+
 ComponentGenerator.prototype.files = function files() {
   var relativePath = 'app/jsx/components/';
   this.template('component.js', relativePath + this.name + '.jsx');
--- component/templates/component.js
+++ component/templates/component.js
@@ -1,5 +1,30 @@
 /*** @jsx React.DOM */
-define(['react'], function(React) {
+'use strict';<% if (moduleLoader === 'browserify') { %>
+/*React with addons*/
+var React = require('react/addons');<% } else { %>
+/*React.js*/
+var React = require('react');<% } %>
+module.exports = React.createClass({
+    getInitialState: function() {
+        return {
+            text: 'Click Me!'
+        };
+    },
+    clickMeUpdate: function(e) {
+        this.setState({
+            text: this.state.text.split('').reverse().join('')
+        });
+    },
+    render: function() {
+        return (
+            <h1 onClick={this.clickMeUpdate}>{this.state.text}</h1>
+        )
+    }
+});<% } else { %>
+define([<% if (includeReactAddons) { %>
+    'react-with-addons', <% } else { %>
+    'react',<% } %>
+], function(React) {
     var <%= name %> = React.createClass({
         getInitialState: function() {
             return {
@@ -17,5 +42,5 @@ define(['react'], function(React) {
             );
         }
     });
-    return <%= name%>;
-});
+    return <%= name %>;
+});<% } %>
