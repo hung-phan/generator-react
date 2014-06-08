a502550c6bd32e87e72dfc6219be6cb2da1001c3
hung-phan <colorvisavn@gmail.com>
2014-06-08 22:00:33 +0700

    update gruntfile template

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -160,18 +160,7 @@ module.exports = function(grunt) {
                     useStrict: true
                 }
             }
-        },<% } else { %>
-
-        //browserify task
-        browserify: {
-          app: {
-            files: { '<%%= yeoman.app %>/scripts/main.js': ['<%%= yeoman.app %>/jsx/main.jsx'] },
-            options: {
-              alias: browserifyAliasConfig,
-              transform: [require('grunt-react').browserify]
-            }
-          }
-        },<% } %>
+        },
 
         //react compilation task
         react: {
@@ -184,7 +173,18 @@ module.exports = function(grunt) {
                 ext: '.js'
               }]
           }
-        },<% if (testFramework === 'jasmine') { %>
+        },<% } else { %>
+
+        //browserify task
+        browserify: {
+          app: {
+            files: { '<%%= yeoman.app %>/scripts/main.js': ['<%%= yeoman.app %>/jsx/main.jsx'] },
+            options: {
+              alias: browserifyAliasConfig,
+              transform: [require('grunt-react').browserify]
+            }
+          }
+        },<% } %><% if (testFramework === 'jasmine') { %>
 
         // Jasmine testing framework configuration options
         jasmine: {
